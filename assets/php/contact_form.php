a53a17cd77e46fcf282d68cd37cc472687ab56ac
hpanchal712 <hpanchal712@gmail.com>
2024-05-26 18:54:30 +0200

    Add files via upload

---
+++ assets/php/contact_form.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
+
+    if (empty($name) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Please complete the form and try again.";
+        exit;
+    }
+
+    $recipient = "hpanchal712@gmail.com";
+    $subject = "New contact from $name";
+    
+    $email_content = "Name: $name\n";
+    $email_content = "Email: $email\n\n";
+    $email_content = "Message:\n$message\n";
+
+    $email_headers = "From: $name <$email>";
+
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        echo "Thank you! Your message has been sent.";
+    } else {
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+
+} else {
+    echo "There was a problem with your submission, please try again.";
+}
+?>
